848a1506d33f1753ec68790077c0e416c3212012
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-24 09:29:31 -0300

    added cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import PaginasRoutes from "./routes/paginas_routes.js"
 
 
@@ -6,9 +7,12 @@ import PaginasRoutes from "./routes/paginas_routes.js"
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
 
 
+app.use(cors({
+    origin: '*'}));
 
 app.use("/api", PaginasRoutes);
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-// Estaba exportado para la mierda
+
 
 
 import app from './app.js';
 import { PORT } from './config.js'
 app.listen(PORT);
 
-/*const cors = require('cors');
-app.use(cors({
-    origin: '*'
-}));*/
