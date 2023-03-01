0fd4f1ac3a71e7928fa2b4e6dfa8874c561ac6bc
Tomas Guida <tomasguida2000@hotmail.com>
2023-02-28 22:08:55 -0300

    added materials and step by step

--- src/controllers/posts_controllers.js
+++ src/controllers/posts_controllers.js
@@ -1,19 +1,16 @@
 import {pool} from '../db.js'
 
-
 export const getPosts = async (req, res) => {
 try{
-const [rows] = await pool.query("SELECT * FROM posts")
+const {rows} = await pool.query("SELECT * FROM posts")
 res.json(rows)
 } catch (error){
     return res.status(500).json({message: "error"})
 }
-};
-
+}
 export const getPost = async(req, res) => {
     try {
-        const { idposts } = req.params;
-        const [rows] = await pool.query('SELECT * FROM posts WHERE idposts = ?',[idposts])
+        const [rows] = await pool.query('SELECT * FROM posts WHERE idposts = ?',[req.params.ids ])
     
         if  (rows.length <= 0) return res.status(404).json({message: "employee not found"})
         res.json(rows[0])
@@ -21,45 +18,39 @@ export const getPost = async(req, res) => {
         return res.status(500).json({message: "error"})
     }
 
-    };
+    }
 
 export const uplPosts = async (req, res) => {
-    const {title, description, img} = req.body
+    const {title, description, img,materials, stepbystep } = req.body
     try {
 
- const [rows] = await pool.query('INSERT INTO posts (title, description, img) VALUES (?, ?,?)',[title, description, img ])
-res.send({idpost : rows.insertId, title, description, img})
+ const [rows] = await pool.query('INSERT INTO posts (title, description, img, ,materials, stepbystep) VALUES (?, ?,?,?,?)',[title, description, img, ,materials, stepbystep ])
+res.send({idpost : rows.insertId, title, description, img, materials, stepbystep})
     } catch (error) {
         return res.status(500).json({message: "error"})
     }
-};
+}
 
-export const updPosts = async(req, res) => {
-
+export const updPosts =async(req, res) => {
+    const {idposts} = req.params
+    const {title, description, img,materials, stepbystep} = req.body
 try {
-    const {idposts} = req.params;
-    const {title, description, img} = req.body;
 
-    const [result] = await pool.query(
-        "UPDATE posts SET title = IFNULL(?,  title) , description = IFNULL(?,  description) , img = IFNULL(?,  img) WHERE idposts = ?",
-         [title, description, img, idposts]
-         );
-    if (result.affectedRows === 0)
-         return res.status(404).json({message :"no encontrado"})
-    const[rows] = await pool.query("SELECT * FROM posts WHERE idposts = ?", [idposts,])
-    res.json(rows[0]);
+    const result = await pool.query("UPDATE posts SET title = IFNULL(?,  title) , description = IFNULL(?,  description) , img = IFNULL(?,  img), ,materials = IFNULL(?,  img), stepbystep = IFNULL(?,  img) WHERE idposts = ?", [title, description, img,,materials, stepbystep, idposts])
+    if (result.affectedRows === 0) return res.status(404).json({message :"no encontrado"})
+    const[rows] = await pool.query("SELECT * FROM posts where idposts = ?", [idposts])
+    res.json(rows)
 } catch (error) {
     return res.status(500).json({message: "error"})    
 }
-};
+}
 
 export const dltPosts = async(req, res) => {
     try {
-        const { idposts } = req.params;
-        const result = await pool.query("DELETE FROM posts WHERE idposts = ?",[idposts]    ) 
+        const result = await pool.query("DELETE FROM posts WHERE idposts = ?",[req.params.ids ]    ) 
 if (result.affectedRows > 0) return res.status(404).json({message: "employee not found"})
 res.sendStatus(204)
     } catch (error) {
         return res.status(500).json({message: "error"})         
     }
-};
+}
