1153768178e63c008bd7584088fdafcd2f9c11fb
Tomas Guida <tomasguida2000@hotmail.com>
2023-03-01 16:34:12 -0300

    fix delete2

--- src/controllers/posts_controllers.js
+++ src/controllers/posts_controllers.js
@@ -49,7 +49,7 @@ export const dltPosts = async(req, res) => {
     try {
         const { idposts } = req.params;
         const result = await pool.query("DELETE FROM posts WHERE idposts = ?",[idposts]    ) 
-if (result.affectedRows > 0) return res.status(404).json({message: "employee not found"})
+if (result.affectedRows > 0) return res.status(404).json({message: "post not found"})
 res.sendStatus(204)
     } catch (error) {
         return res.status(500).json({message: "error"})         
